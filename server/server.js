fb3de88f07685f0bc9ca73b3b980b1191e8d6b8f
Subin Panta <subinpanta99@gmail.com>
2021-05-12 10:10:25 +0545

    Build ready

--- server/server.js
+++ server/server.js
@@ -6,13 +6,13 @@ const port = process.env.PORT || 3000
 const proxy = require('http-proxy-middleware')
 app.use(express.static(publicPath))
 
-// app.use(
-// 	proxy('/api', {
-// 		target: 'https://afternoon-tundra-60689.herokuapp.com/',
-// 		changeOrigin: true,
-// 		pathRewrite: { '^/api': '' }
-// 	})
-// )
+app.use(
+	proxy('/api', {
+		target: 'https://stark-cove-03395.herokuapp.com/',
+		changeOrigin: true,
+		pathRewrite: { '^/api': '' }
+	})
+)
 
 app.get('*', (req, res) => {
 	res.sendFile(path.join(publicPath, 'index.html'))
